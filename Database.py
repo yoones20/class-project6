cc0008bf603a57f39302a1e89e451ebcc0aa27fd
yoones20 <yoonesakbari3@gmail.com>
2023-12-13 18:04:46 +0330

    finish 1

--- Database.py
+++ Database.py
@@ -4,20 +4,24 @@ conn = sqlite3.connect("madrese.db")
 
 corsur = conn.cursor()
 
-corsur.execute ('''
-            CREAT TABLE IF NOT EXIST users
-            id INTEGER PRIMERY KEY
-            fname TEXT
-            lname TEXT 
-            age INTEGER
-            nubmer class INT
-            name teacher TXT  
+corsur.execute('''
+CREATE TABLE IF NOT EXISTS madrese(
+            id INTEGER PRIMARY KEY,
+            fname TEXT,
+            lname TEXT,
+            age INTEGER,
+            nubmer class INT,
+            name teacher TXT, 
+)
 ''')
 
 
-more_users = ("1", "MAMMAD", "AKBARI", "20", "102", "MALEKI")
+more_users = ("1", "MAMMAD", "AKBARI", "20", "102", "MALEKI"),("2", "ali", "alian", "23", "maleki")
 corsur.executemany("INSERT INTO users values(?, ?, ?, ?, ?, ?);", more_users)
 
-
+corsur.execute("SELECT * FROM users")
+one_result = corsur.fetchone()
+print(one_result)
 conn.commit()
+conn.close
 
