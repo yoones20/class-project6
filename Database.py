9a4a06963a18094ee87e55cf4891a3390037e51d
yoones20 <yoonesakbari3@gmail.com>
2023-12-13 17:21:16 +0330

    EXECUTE

--- Database.py
+++ Database.py
@@ -4,3 +4,9 @@ conn = sqlite3.connect("madrese.db")
 
 corsur = conn.cursor()
 
+corsur.execute ('''
+            CREAT TABLE IF NOT EXIST madrese
+            id INTEGER PRIMERY KEY
+            name TEXT NOT NUL
+            age INTEGER   
+''')
